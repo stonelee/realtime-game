541471b7fd47280cf9264e8ef17c41ec2782ea12
stonelee <istonelee@gmail.com>
2013-03-20 16:30:45 +0800

    use process.env.PORT

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ var express = require('express'),
   server = require('http').createServer(app),
   io = require('socket.io').listen(server);
 
-server.listen(3000);
+var port = process.env.PORT || 3000;
+server.listen(port);
 
 app.use(express.static(__dirname + '/public'));
 
