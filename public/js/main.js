0341cb1942ec66e4a6e6d65d7026848d0624d2a5
stonelee <istonelee@gmail.com>
2013-03-20 09:54:40 +0800

    add socket.io

--- public/js/main.js
+++ public/js/main.js
@@ -29,4 +29,11 @@ $(function() {
       $person.css('left', maxLeft);
     }
   })
+
+  var socket = io.connect('http://localhost');
+  socket.on('news', function (data) {
+    console.log(data);
+    socket.emit('my other event', { my: 'data' });
+  });
+
 });
